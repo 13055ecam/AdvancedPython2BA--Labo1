06f446bb5a24de1e06f236caf1341ba612ea127b
Alexandre Lenaerts <13055@ecam.be>
2016-02-04 00:07:51 +0100

    fixature utils

--- test_utils.py
+++ test_utils.py
@@ -7,13 +7,17 @@ import utils
 
 class TestUtils(unittest.TestCase):
     def test_fact(self):
-    	self.assertEqual(utils.fact(0) , 3)
+    	self.assertEqual(utils.fact(0) , 1)
+    	self.assertRaises(ValueError, utils.fact, -1)
+    	self.assertEqual(utils.fact(0), 1)
+    	self.assertEqual(utils.fact(5), 120)
     
     def test_roots(self):
-        self.assertEqual(utils.roots(1,5,8), (0,9/1,3/4))
-    
+    	self.assertEqual(utils.roots(1, 2, 1), (-1))
+		self.assertEqual(utils.roots(1, 0, 2), ())
+   
     def test_integrate(self):
-    	self.assertEqual(utils.integrate(x,4,1), 3)
+    	self.assertAlmostEquals(utils.integrate('x', 0, 3), 0.5)
 
 if __name__ == '__main__':
     suite = unittest.TestLoader().loadTestsFromTestCase(TestUtils)
