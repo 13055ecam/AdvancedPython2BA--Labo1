245d291e70a23d9c2d6cbcda1d97c7fe18c8fba7
Alexandre Lenaerts <13055@ecam.be>
2016-02-21 13:36:47 +0100

    update

--- utils.py
+++ utils.py
@@ -66,5 +66,5 @@ def integrate(function, lower, upper):
 if __name__ == '__main__':
     print(fact(5))
     print(roots(1, 0, 1))
-    print(integrate('x ** 2 - 1', -1, 1))
+    print(integrate("x",0,4))
     print(integrate('x ** 2 - 1', -1, 1))
